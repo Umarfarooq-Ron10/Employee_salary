5348c747eeb1822baccbf205d06a24efcf4abe4e
Umarfarooq J Ron <umarron.10@gmail.com>
2025-11-10 16:24:40 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -3,7 +3,7 @@ if len(sys.argv)==2:
   script_salary=sys.argv[0]
   salary=sys.argv[1]
 else:
-  scrip_salary=sya.argv[0]
+  script_salary=sys.argv[0]
   salary=float(sys.argv[1])
   bonus=0.10*salary
   total_salary=salary*bonus
