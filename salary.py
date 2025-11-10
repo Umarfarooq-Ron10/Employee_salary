1a5a40a550d0bbf0d4939371befb598b1bcdc5d9
Umarfarooq J Ron <umarron.10@gmail.com>
2025-11-10 16:23:48 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,5 +1,11 @@
-salary=float(sys.argv[1])
-bonus=0.10*salary
-total_salary=salary*bonus
+import sys
+if len(sys.argv)==2:
+  script_salary=sys.argv[0]
+  salary=sys.argv[1]
+else:
+  scrip_salary=sya.argv[0]
+  salary=float(sys.argv[1])
+  bonus=0.10*salary
+  total_salary=salary*bonus
 print("Bonus Amount:",bonus)
 print("Total Salary:",total_salary)
